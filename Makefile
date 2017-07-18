0337ba019534dc4798da2283f20610f86b233e39
Peter Valdez <peter@nycmesh.net>
2017-07-18 18:22:19 -0400

    Commit to save code that illustrates a problem with the data

--- Makefile
+++ Makefile
@@ -4,7 +4,17 @@ PROJECT_NAME = new-python-project
 all: build run
 
 build:
-	docker build -t $(PROJECT_NAME) image/
+	@docker build -t $(PROJECT_NAME) image/
 
 run:
-	docker run --rm $(PROJECT_NAME)
+	@docker run --rm -v $(shell pwd)/data:/data $(PROJECT_NAME)
+
+shell:
+	@docker run --rm -it -v $(shell pwd)/data:/data $(PROJECT_NAME) sh
+
+silent-build:
+	@docker build -t $(PROJECT_NAME) image/ >/dev/null 2>&1
+
+keep-running:
+	@#Relies on the vim-hook script to write to output.txt
+	@watch -n0.5 cat output.txt
--- image/filesystem/root/main.py
+++ image/filesystem/root/main.py
@@ -1,3 +1,56 @@
 #!/usr/bin/env python
+import csv
+from pprint import pprint
+from collections import defaultdict
+from functools import partial
 
-print("Ready to go.")
+import geojson
+
+
+FIELDS = [
+    'Line',
+    'Station Name',
+    'Station Latitude',
+    'Station Longitude',
+    'North South Street',
+    'East West Street',
+    'Entrance Latitude',
+    'Entrance Longitude',
+]
+
+
+def pluck(d):
+    return {f: d[f] for f in FIELDS}
+
+
+# Load the data
+with open('/data/nyc-subway-entrance.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    exits = list(reader)
+
+# pprint(exits[0])
+
+# print('----------------------------------------------')
+
+listdict_creator = partial(defaultdict, list)
+subway_lines = defaultdict(listdict_creator)
+
+for exit in exits:
+    line, station_name = exit['Line'], exit['Station Name']
+    subway_lines[line][station_name].append(exit)
+
+
+# pprint([pluck(x) for x in subway_lines['6 Avenue']['East Broadway']])
+
+# print('----------------------------------------------')
+
+for line, stations in subway_lines.items():
+    for station_name, exits in stations.items():
+        station_latitudes = [x['Station Latitude'] for x in exits]
+        entrance_latitudes = [x['Entrance Latitude'] for x in exits]
+        if len(set(station_latitudes)) > 1:
+            print(station_name)
+            print(station_latitudes)
+            print(entrance_latitudes)
+            # pprint(exits)
+            print()
