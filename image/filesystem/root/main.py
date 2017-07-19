155cd515021774cc2ab29f5bf0db90a400138923
Peter Valdez <peter@nycmesh.net>
2017-07-18 20:24:08 -0400

    Break code into functions

--- image/filesystem/root/main.py
+++ image/filesystem/root/main.py
@@ -7,6 +7,7 @@ from functools import partial
 import geojson
 
 
+DATA_FILE = '/data/nyc-subway-entrance.csv'
 FIELDS = [
     'Line',
     'Station Name',
@@ -23,34 +24,41 @@ def pluck(d):
     return {f: d[f] for f in FIELDS}
 
 
-# Load the data
-with open('/data/nyc-subway-entrance.csv', 'r') as f:
-    reader = csv.DictReader(f)
-    exits = list(reader)
-
-# pprint(exits[0])
-
-# print('----------------------------------------------')
-
-listdict_creator = partial(defaultdict, list)
-subway_lines = defaultdict(listdict_creator)
-
-for exit in exits:
-    line, station_name = exit['Line'], exit['Station Name']
-    subway_lines[line][station_name].append(exit)
+def load_all_exits():
+    with open(DATA_FILE) as f:
+        reader = csv.DictReader(f)
+        exits = list(reader)
+    return exits
 
 
-# pprint([pluck(x) for x in subway_lines['6 Avenue']['East Broadway']])
+def group_by_line_and_station(exits):
+    listdict_creator = partial(defaultdict, list)
+    subway_line_stations = defaultdict(listdict_creator)
 
-# print('----------------------------------------------')
+    for exit in exits:
+        line, station_name = exit['Line'], exit['Station Name']
+        subway_line_stations[line][station_name].append(exit)
 
-for line, stations in subway_lines.items():
-    for station_name, exits in stations.items():
-        station_latitudes = [x['Station Latitude'] for x in exits]
-        entrance_latitudes = [x['Entrance Latitude'] for x in exits]
-        if len(set(station_latitudes)) > 1:
-            print(station_name)
-            print(station_latitudes)
-            print(entrance_latitudes)
-            # pprint(exits)
-            print()
+    return subway_line_stations
+
+
+def check_station_latitudes(subway_line_stations):
+    for line, stations in subway_line_stations.items():
+        for station_name, exits in stations.items():
+            station_latitudes = [x['Station Latitude'] for x in exits]
+            entrance_latitudes = [x['Entrance Latitude'] for x in exits]
+
+            if len(set(station_latitudes)) > 1:
+                print(station_name)
+                print(station_latitudes)
+                print(entrance_latitudes)
+                print()
+
+
+def main():
+    exits = load_all_exits()
+    subway_line_stations = group_by_line_and_station(exits)
+
+
+if __name__ == '__main__':
+    main()
